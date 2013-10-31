a8badb02c6289e4d1d6cdd01abdb9b24efc83990
Tysonzero <danielspencersmith@gmail.com>
2013-10-31 09:25:04 -0600

    Add key array to keyBoardInput
    stores which keys are held down

--- scripts/vars/keyBoardInput.js
+++ scripts/vars/keyBoardInput.js
@@ -1,4 +1,7 @@
 var keyBoardInput = {
+  //array storing what keys are held down
+  key:[],
+
   //creates keyboard listener
   initialize:function() {
     this._bind_keys();
@@ -7,7 +10,12 @@ var keyBoardInput = {
   _bind_keys:function() {
     //called whenever a key is pressed
     window.onkeydown = function(e) {
-
+      keyBoardInput.key[e.keyCode] = true;
+    };
+    
+    //called whenever a key is released
+    window.onkeyup = function(e) {
+      keyBoardInput.key[e.keyCode] = false;
     };
   },
 };
