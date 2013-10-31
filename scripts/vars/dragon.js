bbc9a0f86ccb811d095efed781aae6c9b64d4c54
Tysonzero <danielspencersmith@gmail.com>
2013-10-31 12:14:25 -0600

    Add dragon position and velocity

--- scripts/vars/dragon.js
+++ scripts/vars/dragon.js
@@ -1,4 +1,16 @@
 var dragon = {
+  //dragon position
+  pos = {
+    x:0,
+    y:0,
+  },
+  
+  //dragon velocity
+  speed = {
+    x:0,
+    y:0,
+  },
+  
   //variable manipulation
   update:function() {
   
