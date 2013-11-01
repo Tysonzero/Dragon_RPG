a1f2a1d9f280c49a246f0c4b13dc9002052c67e2
Tysonzero <danielspencersmith@gmail.com>
2013-11-01 13:26:25 -0600

    Add dragon setup and image

--- scripts/vars/dragon.js
+++ scripts/vars/dragon.js
@@ -11,6 +11,14 @@ var dragon = {
     y:0,
   },
   
+  //dragon image
+  image:new Image(),
+  
+  //initial set up
+  setup:function() {
+    this.image.src = "images/dragon.png";
+  },
+  
   //variable manipulation
   update:function() {
   
