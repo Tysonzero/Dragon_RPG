b19e4819cb08d1267bc4bf6c37868e4b9fd93f93
Tysonzero <danielspencersmith@gmail.com>
2013-11-11 09:43:36 -0700

    Make camera draw from center of screen
    Also fix camera.size

--- scripts/vars/camera.js
+++ scripts/vars/camera.js
@@ -7,13 +7,13 @@ var camera = {
   
   //camera view area
   size:{
-    x:800,
-    y:600,
+    x:1000,
+    y:700,
   },
   
   //convert true position to position on screen
   getPos:function(pos) {
-    return {x:(pos.x - this.pos.x) * game.width / this.size.x, y:(pos.y - this.pos.y) * game.height / this.size.y};
+    return {x:((pos.x - this.pos.x) * game.width / this.size.x) + game.width/2, y:((pos.y - this.pos.y) * game.height / this.size.y) + game.height/2};
   },
   
   //convert true size to size on screen
