6a90cc40ed334e0b295ff5ca61971f54db08883e
Tysonzero <danielspencersmith@gmail.com>
2013-11-11 09:29:36 -0700

    Add camera getPos and getSize

--- scripts/vars/camera.js
+++ scripts/vars/camera.js
@@ -10,4 +10,14 @@ var camera = {
     x:800,
     y:600,
   },
+  
+  //convert true position to position on screen
+  getPos:function(pos) {
+    return {x:(pos.x - this.pos.x) * game.width / this.size.x, y:(pos.y - this.pos.y) * game.height / this.size.y};
+  },
+  
+  //convert true size to size on screen
+  getSize:function(size) {
+    return {x:size.x * game.width / this.size.x, y:size.y * game.height / this.size.y};
+  },
 };
