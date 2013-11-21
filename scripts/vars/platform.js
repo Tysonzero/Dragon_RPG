49372490478631ba17edf8af936fb40f5dacfc41
Tysonzero <danielspencersmith@gmail.com>
2013-11-21 12:49:09 -0700

    Add platform.create

--- scripts/vars/platform.js
+++ scripts/vars/platform.js
@@ -3,9 +3,14 @@ var platform = {
   instances:[],
 
   //Platform prototype
-	Platform:function(pos, size){
+	Platform:function(pos, size) {
     //objects with x and y attributes
 		this.pos = pos;
 		this.size = size;
 	},
+  
+  //creates a new platform
+  create:function(pos, size) {
+    this.instances[this.instances.length] = new this.Platform(pos, size);
+  },
 };
