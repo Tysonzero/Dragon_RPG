6d1a706e0141064481fe68412d1d2c63e35c52e0
Tysonzero <danielspencersmith@gmail.com>
2013-11-22 13:06:01 -0700

    Create platform.draw
    Draws all platforms

--- scripts/vars/platform.js
+++ scripts/vars/platform.js
@@ -9,6 +9,17 @@ var platform = {
 		this.size = size;
 	},
   
+  //drawing to screen
+  draw:function() {
+    //draw all platforms
+    for (i = 0; i < this.instances.length; i++) {
+      pos = camera.getPos(this.instances[i].pos);
+      size = camera.getSize(this.instances[i].size);
+      game.ctx.fillStyle="#000000";
+      game.ctx.fillRect(pos.x - size.x/2, pos.y - size.y/2, size.x, size.y);
+    }
+  },
+  
   //creates a new platform
   create:function(pos, size) {
     this.instances[this.instances.length] = new this.Platform(pos, size);
