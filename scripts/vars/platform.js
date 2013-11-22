b635e88bb64ef28be6d8cc30854d6cb2f6bec0aa
Tysonzero <danielspencersmith@gmail.com>
2013-11-22 13:49:12 -0700

    Create platform.collide
    Currently only checks for right said collision and alerts if a collision
    occurs

--- scripts/vars/platform.js
+++ scripts/vars/platform.js
@@ -9,6 +9,11 @@ var platform = {
 		this.size = size;
 	},
   
+  //creates a new platform
+  create:function(posX, posY, sizeX, sizeY) {
+    this.instances[this.instances.length] = new this.Platform({x:posX, y:posY}, {x:sizeX, y:sizeY});
+  },
+  
   //drawing to screen
   draw:function() {
     //draw all platforms
@@ -20,8 +25,23 @@ var platform = {
     }
   },
   
-  //creates a new platform
-  create:function(posX, posY, sizeX, sizeY) {
-    this.instances[this.instances.length] = new this.Platform({x:posX, y:posY}, {x:sizeX, y:sizeY});
-  },
+  //adjust object if a collision occurs
+  collide:function(pos, size) {
+    //object containing adjustments to make to object
+    var collision = {
+      left:0,
+      right:0,
+      up:0,
+      down:0,
+    }
+  
+    //check if object collides with any platform
+    for (i = 0; i < this.instances.length; i++) {
+      if (pos.y + size.y/2 > this.instances[i].pos.y - this.instances[i].size.y/2 && pos.y - size.y/2 < this.instances[i].pos.y + this.instances[i].size.y/2) {
+        if (pos.x + size.x/2 >= this.instances[i].pos.x - this.instances[i].size.x/2 && pos.x + size.x/2 <= this.instances[i].pos.x + this.instances[i].size.x/2) {
+          alert("RIGHT SIDE COLLISION");
+        }
+      }
+    }
+  }
 };
