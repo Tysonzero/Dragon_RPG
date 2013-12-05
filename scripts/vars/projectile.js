51b1248c06a15032ffa1fdc4af515cb0922e9823
Tysonzero <danielspencersmith@gmail.com>
2013-12-05 12:14:54 -0700

    Create projectile.draw

--- scripts/vars/projectile.js
+++ scripts/vars/projectile.js
@@ -14,4 +14,15 @@ var projectile = {
   create:function(posX, posY, sizeX, sizeY, speedX, speedY) {
     this.instances[this.instances.length] = new this.Projectile({x:posX, y:posY}, {x:sizeX, y:sizeY}, {x:speedX, y:speedY});
   },
+  
+  //drawing to screen
+  draw:function() {
+    //draw all projectiles
+    for (i = 0; i < this.instances.length; i++) {
+      pos = camera.getPos(this.instances[i].pos);
+      size = camera.getSize(this.instances[i].size);
+      game.ctx.fillStyle="#FF0000";
+      game.ctx.fillRect(pos.x - size.x/2, pos.y - size.y/2, size.x, size.y);
+    }
+  },
 };
