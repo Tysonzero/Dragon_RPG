6323a113d1b64e4fdeffb1ba2d22e3242403893e
DanielSpencerSmith <danielsmith@Daniels-MacBook-Air.local>
2013-12-05 10:03:10 -0700

    Add Projectile prototype

--- scripts/vars/projectile.js
+++ scripts/vars/projectile.js
@@ -1,3 +1,9 @@
 var projectile = {
-	
+	//Projectile prototype
+	Projectile:function(pos, size, speed) {
+		//objects with x and y attributes
+		this.pos = pos;
+		this.size = size;
+		this.speed = speed;
+	}
 };
